e820e2eb76d3a7e98b5887380f0ca8fdb1160ddb
kamilkodzi <bizkamiladamski@gmail.com>
2023-11-29 20:54:41 +0100

    exporting default fn

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 100,
+  blue: 244,
+  green: 120,
+};
+
+console.log(color);
+
+export default color
