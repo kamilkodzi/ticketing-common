41a3667e90d113526522b7d7c0abc8a0202d2918
kamilkodzi <bizkamiladamski@gmail.com>
2024-01-07 11:47:59 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,6 @@ export * from './events/base-publisher'
 export * from './events/subjects'
 export * from './events/ticket-created-events'
 export * from './events/ticket-updated-event'
-
 export * from './events/types/order-status'
+export * from './events/order-cancelled-event'
+export * from './events/order-created-event'
